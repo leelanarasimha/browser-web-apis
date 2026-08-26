95214e9cbd7053da4660d11132a26a6423ceefb4
leelanarasimha <leela.narsimha@gmail.com>
2026-08-27 00:35:23 +0530

    initial commit

---
+++ service-worker/offline-todo/service-worker.js
@@ -0,0 +1,73 @@
+const Version = 'v3';
+const cache_name = `tasks-${Version}`;
+
+globalThis.addEventListener('install', (event) => {
+  console.log('Service Worker is installed', Version);
+  event.waitUntil(caches.open(cache_name));
+  globalThis.skipWaiting();
+});
+
+globalThis.addEventListener('activate', (event) => {
+  console.log('service worker activated', Version);
+  event.waitUntil(Promise.all([deleteCaches(), globalThis.clients.claim()]));
+});
+
+async function deleteCaches() {
+  const cacheNames = await caches.keys();
+  for (let cache of cacheNames) {
+    if (cache !== cache_name) await caches.delete(cache);
+  }
+  console.log('available caches', await caches.keys());
+}
+
+globalThis.addEventListener('fetch', (event) => {
+  const url = event.request.url;
+
+  if (!url.startsWith('https://jsonplaceholder.typicode.com/todos/')) {
+    return;
+  }
+  event.respondWith(handleTodoRequest(event));
+});
+
+async function handleTodoRequest(event) {
+  const cache = await openCache();
+  let message = 'Cache Hit';
+
+  let response = await cache.match(event.request);
+
+  if (!response) {
+    message = 'Network';
+    response = await fetch(event.request);
+    await cache.put(event.request, response.clone());
+  }
+
+  const client = await globalThis.clients.get(event.clientId);
+  client.postMessage({
+    type: 'CACHE_MESSAGE',
+    message
+  });
+
+  return response;
+}
+
+function openCache() {
+  return caches.open(cache_name);
+}
+
+globalThis.addEventListener('message', (event) => {
+  if (event.data.type === 'DELETE_CACHE') {
+    const todoId = event.data.todoId;
+    deleteCache(todoId);
+  }
+});
+
+function generateRequestUrl(todoId) {
+  const url = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
+  return new Request(url);
+}
+
+async function deleteCache(todoId) {
+  const request = generateRequestUrl(todoId);
+  const cache = await openCache();
+  await cache.delete(request);
+}
---
+++ service-worker/offline-todo/todo.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', init);
+
+navigator.serviceWorker.addEventListener('message', (event) => {
+  if (event.data.type === 'CACHE_MESSAGE') {
+    renderMessage(event.data.message);
+  }
+});
+
+async function init() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js');
+  }
+
+  const cache = await caches.open('tasks-v1');
+  const todoButtonElement = document.querySelector('#todo-button');
+  const deleteButtonElement = document.querySelector('#delete-button');
+  todoButtonElement.addEventListener('click', () => getTodoValueEvent(cache));
+  deleteButtonElement.addEventListener('click', () => deleteTodoEvent(cache));
+}
+
+async function deleteTodoEvent(cache) {
+  const todoId = getTodoId();
+  navigator.serviceWorker.controller.postMessage({
+    type: 'DELETE_CACHE',
+    todoId
+  });
+}
+
+async function getTodoValueEvent(cache) {
+  const todoId = getTodoId();
+  const request = generateRequestUrl(todoId);
+  const response = await getTodoResponse(request);
+  const data = await response.json();
+  renderData(data);
+}
+
+function renderMessage(message) {
+  const messageElement = document.querySelector('#message');
+  messageElement.textContent = message;
+}
+
+function renderData(data) {
+  const containerElement = document.querySelector('#todo-container');
+  containerElement.innerHTML = '';
+  const divElement = document.createElement('div');
+  let values = ['id', 'title', 'completed'];
+
+  for (let value of values) {
+    const element = document.createElement('div');
+    element.textContent = `${value}: ${data[value]}`;
+    divElement.append(element);
+  }
+  containerElement.append(divElement);
+}
+
+function getTodoId() {
+  const todoSelectElement = document.querySelector('#todo-select');
+  const todoId = todoSelectElement?.value;
+  return todoId;
+}
+
+function getTodoResponse(request) {
+  console.log(navigator.serviceWorker.controller);
+  return fetch(request);
+}
+
+function generateRequestUrl(todoId) {
+  const url = `https://jsonplaceholder.typicode.com/todos/${todoId}`;
+  return new Request(url);
+}
+function getCache(cache, request) {
+  return cache.match(request);
+}
+async function saveToCache(cache, request, response) {
+  await cache.put(request, response.clone());
+}
